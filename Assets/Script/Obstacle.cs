cf92a7d2c23ab979c0a5beb470866ba689dcbb67
Lemorz56 <sebbe_xbox@hotmail.com>
2019-12-23 00:20:33 +0100

    Intial Commit

---
+++ Assets/Script/Obstacle.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacle : MonoBehaviour{
+
+    public int damage;
+    public float speed;
+    public GameObject effect;
+
+    private CamShake shake;
+
+    private void Start()
+    {
+        shake = GameObject.FindGameObjectWithTag("ScreenShake").GetComponent<CamShake>();
+    }
+
+    private void Update()
+    {
+        transform.Translate(Vector2.left * speed * Time.deltaTime);
+    }
+
+    //If istrigger check = run this function at collide. Use paramter type Collider2D called other.
+    void OnTriggerEnter2D(Collider2D other) 
+    {
+        if (other.CompareTag("Player"))
+        {
+            shake.camShake();
+            Debug.Log(shake);
+            Instantiate(effect, transform.position, Quaternion.identity);
+
+            // Player takes damage ! 
+            other.GetComponent<Player>().health -= damage;
+            Debug.Log(other.GetComponent<Player>().health);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Script/Player.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//for death control
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+    //void Start()
+    //{  
+    //}
+    private Vector2 targetPos;
+    public float yIncrement;
+
+    public float speed;
+    public float maxHeight;
+    public float minHeight;
+
+    public int health = 3;
+    public GameObject effect;
+
+    //public Animator camAnim;
+
+    private CamShake shake;
+
+    private void Start()
+    {
+        shake = GameObject.FindGameObjectWithTag("ScreenShake").GetComponent<CamShake>();
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        if (health <= 0){
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+
+        transform.position = Vector2.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+
+        if (Input.GetKeyDown(KeyCode.UpArrow) && transform.position.y < maxHeight)
+        {
+            //camAnim.SetTrigger("shake");
+            shake.camShake();
+
+            targetPos = new Vector2(transform.position.x, transform.position.y + yIncrement);
+            Instantiate(effect, transform.position, Quaternion.identity);
+
+            //Below we set the position of target to be targetPos which we do above
+            //transform.position = targetPos;
+
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow) && transform.position.y > minHeight)
+        {
+            //camAnim.SetTrigger("shake");
+            shake.camShake();
+
+            targetPos = new Vector2(transform.position.x, transform.position.y - yIncrement);
+            Instantiate(effect, transform.position, Quaternion.identity);
+
+            //Below we set the position of target to be targetPos which we do above
+            //transform.position = targetPos;
+
+        }
+    }
+}
